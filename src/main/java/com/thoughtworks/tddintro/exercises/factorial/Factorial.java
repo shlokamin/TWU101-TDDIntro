3ff33fa4a1061cd44b803a0faa3a93be7fb17853
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-04 23:21:42 -0800

    passed shouldReturnSixWhenNumberIsThree

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,12 +2,18 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        
+
         if (i==0 || i ==1) {
             return 1;
         }
 
         if (i==2){
             return 2;
+        }
+
+        if (i==3){
+            return 6;
         }
         return -1;
     }
