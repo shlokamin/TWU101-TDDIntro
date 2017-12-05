7d4c1d6398f46f254e76d82c5a7eb46566361292
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-04 23:24:01 -0800

    passed shouldThrowIllegalArgumentExceptionWhenNumberIsNegative

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,7 +2,11 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        
+
+        if (i<0){
+            throw new IllegalArgumentException("number cannot be negative");
+        }
+
 
         if (i==0 || i ==1) {
             return 1;
